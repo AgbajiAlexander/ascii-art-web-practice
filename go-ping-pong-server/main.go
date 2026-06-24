052e7244281e8c317f15a234330662ae4ce61292
AgbajiAlexander <alexanderogah@gmail.com>
2026-06-24 17:05:17 +0100

    my first server in Go

--- go-ping-pong-server/main.go
+++ go-ping-pong-server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -9,8 +10,42 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "pong")
 }
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "Guest"
+	}
+
+	fmt.Fprintf(w, "Hello, %s!", name)
+
+}
+
+func countHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		fmt.Print(w, "Send a POST request with text to count words")
+		return
+	}
+
+	if r.Method != http.MethodPost {
+		bodyBytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read Body", http.StatusInternalServerError)
+			return
+		}
+		fmt.Printf(w, "Character count: %d", len(bodyBytes))
+		return
+	}
+}
+
 func main() {
 	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/hello", helloHandler)
+
 	fmt.Print("Starting server on :8080...")
 	http.ListenAndServe(":8080", nil)
 }
